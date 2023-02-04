f156557a9d0b5a5135d86f77525373da74645de4
Sujoy Sarkar <sujoysarkar96@gmail.com>
2023-02-04 17:35:03 -0500

    Commit Math Calculator Code

---
+++ src/Math1/Math1.java
@@ -0,0 +1,21 @@
+package Math1;
+
+public class Math1 {
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 5; 
+		
+		int sum = x + y;
+		int sub = x - y;
+		int mul = x * y; 
+		int div = x / y;
+		
+		System.out.println("The sum of the two numbers is: " + sum);
+		System.out.println("");
+		System.out.println("When you subtract the two numbers, you get: " + sub);
+		System.out.println("");
+		System.out.println("After you multiply the two number, the result is: " + mul);
+		System.out.println("");
+		System.out.println("After diving the two numbers, the result is: " + div);
+	}
+}
